ef21e514da0cd0ead1d2c7adcdd29af061369423
GOgo8Go <153721248+GOgo8Go@users.noreply.github.com>
2026-01-30 11:55:38 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,12 +29,9 @@ export default defineConfig({
 
 		preload: {
 	      hover: true,           // 保留 hover 预载（大多数人想要的）
-	      hover: {               // ← 推荐加这个对象写法
-	       delay: 120,          // 建议 80~200ms，根据你鼠标移动速度调
-	          // throttle: 100     // 部分版本支持（新版 preload 插件可能有）
-	      },
-	        // visible: true,      // 视口内自动预载（可选，关掉可进一步省流量）
+		  visible: true,      // 视口内自动预载（可选，关掉可进一步省流量）
 	    },
+		
 		accessibility: true,
 		updateHead: true,
 		updateBodyClass: false,
